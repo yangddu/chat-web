b79b8dc15ad638de30c33091bba9f093041a9ae5
jooyoung <jooyoung@pood.pet>
2022-09-28 21:54:42 +0900

    add: login layout

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -3,8 +3,16 @@ import './Login.scss';
 
 export default function Login() {
   return (
-    <div>
-      <img src="https://swit.io/assets/images/lib/ui/illust/normal-welcome-light.svg" />
+    <div className="wrapper">
+      <div className="vertical-wrapper top"></div>
+      <div className="vertical-wrapper middle">
+        <img src="https://swit.io/assets/images/lib/ui/illust/normal-welcome-light.svg" />
+        <div className="header-layout">협업의 시작, we chat!</div>
+        <div className="button-layout">
+          <button className="free-standard">Free/Standard</button>
+          <button className="advanced">Advanced</button>
+        </div>
+      </div>
     </div>
   );
 }
